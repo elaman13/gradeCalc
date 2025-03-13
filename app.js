6b04dd0a290432b7cf0b752126e7622d5150d778
elaman13 <elaman99619@gmail.com>
2025-03-13 14:29:20 +0300

    Add a new UI, with fixing some bugs in js

--- app.js
+++ app.js
@@ -2,46 +2,42 @@ function calcGrade() {
   const grades = document.getElementsByClassName("grade");
   const crh = document.getElementsByClassName("crh");
 
-  const gradeArr = [];
-  const crhArr = [];
-
-  for (let i = 0; i < grades.length; i++) {
-    gradeArr.push(grades[i].value);
-  }
-
-  for (let i = 0; i < crh.length; i++) {
-    crhArr.push(Number(crh[i].value));
-  }
-
   let totalCrh = 0;
   let totalAddedGrade = 0;
 
-  for (let i = 0; i < crhArr.length; i++) {
-    if (gradeArr[i] != "" && crhArr[i] != "") {
-      if (
-        gradeArr[i].toUpperCase() === "A" ||
-        gradeArr[i].toUpperCase() === "A+"
-      ) {
-        totalAddedGrade += 4 * crhArr[i];
-      } else if (gradeArr[i].toUpperCase() === "A-") {
-        totalAddedGrade += 3.75 * crhArr[i];
-      } else if (gradeArr[i].toUpperCase() === "B+") {
-        totalAddedGrade += 3.5 * crhArr[i];
-      } else if (gradeArr[i].toUpperCase() === "B") {
-        totalAddedGrade += 3.0 * crhArr[i];
-      } else if (gradeArr[i].toUpperCase() === "B-") {
-        totalAddedGrade += 2.75 * crhArr[i];
-      } else if (gradeArr[i].toUpperCase() === "C+") {
-        totalAddedGrade += 2.5 * crhArr[i];
-      } else if (gradeArr[i].toUpperCase() === "C") {
-        totalAddedGrade += 2 * crhArr[i];
-      } else if (gradeArr[i].toUpperCase() === "D") {
-        totalAddedGrade += 1 * crhArr[i];
-      }
+  for (let i = 0; i < crh.length; i++) {
+    let crhValue = Number(crh[i].value) || 0;
+    let gradeValue = grades[i].value;
 
-      totalCrh += Number(crhArr[i]);
+    if (gradeValue === "A" || gradeValue === "A+") {
+      totalAddedGrade += 4 * crhValue;
+    } else if (gradeValue === "A-") {
+      totalAddedGrade += 3.75 * crhValue;
+    } else if (gradeValue === "B+") {
+      totalAddedGrade += 3.5 * crhValue;
+    } else if (gradeValue === "B") {
+      totalAddedGrade += 3.0 * crhValue;
+    } else if (gradeValue === "B-") {
+      totalAddedGrade += 2.75 * crhValue;
+    } else if (gradeValue === "C+") {
+      totalAddedGrade += 2.5 * crhValue;
+    } else if (gradeValue === "C") {
+      totalAddedGrade += 2 * crhValue;
+    } else if (gradeValue === "C-") {
+      totalAddedGrade += 1.75 * crhValue;
+    } else if (gradeValue === "D") {
+      totalAddedGrade += 1 * crhValue;
+    } else if (gradeValue === "F") {
+      totalAddedGrade += 0;
     }
+
+    totalCrh += crhValue;
   }
+
+  console.log("totalCrh: ", totalCrh);
+  console.log("totalAddedGrade: ", totalAddedGrade);
+
   let finalGrade = totalAddedGrade / totalCrh;
+  console.log(finalGrade);
   document.getElementById("result").innerHTML = finalGrade.toFixed(2);
 }
